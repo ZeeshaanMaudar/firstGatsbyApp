4379e924fe0eea191dec1c7405f80579bcface28
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-01-27 20:38:44 +0400

    deploy to github

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
-  pathPrefix: `/firstGatsbyApp`,
+  // pathPrefix: `/firstGatsbyApp`,
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
